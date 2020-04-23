83269de98359ebfc9fe399e35811d78f36a8bf30
Halit Batur <halitfuatbatur@gmail.com>
2020-04-23 18:52:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply);
+let random = Math.floor(Math.random()*10)+1;
+console.log(random);
+let mod = 10%6;
+console.log(mod);
+let max = Math.max (20,10,5,6,8);
+console.log(max);
